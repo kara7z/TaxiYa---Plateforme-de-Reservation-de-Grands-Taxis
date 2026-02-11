e57be803f241238def694e82453cb7db18f73f65
sabri-rihab <rihabsabri21@gmail.com>
2026-02-11 09:05:46 +0100

    creation des migrations

---
+++ database/migrations/2026_02_10_232439_create_seats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('seat_number');
+            $table->enum('type', ['front', 'middle', 'back']);
+
+            $table->foreignId('taxi_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
