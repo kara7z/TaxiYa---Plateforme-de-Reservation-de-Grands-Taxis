4fd0c4027560dea0d2ec6257d8220c48975d6498
kuklinski <mohammedmehdisaibat@gmail.com>
2026-02-13 12:26:22 +0100

    add trip search engine

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\TripController;
 use App\Http\Controllers\Auth\DriverController;
 use App\Http\Controllers\Auth\SessionsController;
+use App\Http\Controllers\TripSearchController;
+use App\Models\City;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +19,11 @@ Route::view('/', 'pages.home')->name('home');
 Route::view('/welcome', 'welcome')->name('welcome');
 
 Route::prefix('trips')->name('trips.')->group(function () {
-    Route::view('/search', 'pages.search')->name('search');
-    Route::view('/results', 'pages.results')->name('results');
-    Route::view('/{trip}', 'pages.trip.show')->name('show');
+    Route::get('/search', function() {
+        return view('pages.search', ['cities' => City::all()]);
+    })->name('search');
+    Route::get('/results', [TripSearchController::class, 'search'])->name('results');
+    Route::get('/{trip}', [TripSearchController::class, 'show'])->name('show');
 });
 
 /*
