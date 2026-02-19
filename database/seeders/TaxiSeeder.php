6b859b4b76204c5a8ff5559b6299a87cd94650dd
kara7z <kara.ossama12@gmail.com>
2026-02-19 15:39:07 +0000

    Search Fixing Problems

--- database/seeders/TaxiSeeder.php
+++ database/seeders/TaxiSeeder.php
@@ -2,20 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Seat;
 use App\Models\Taxi;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class TaxiSeeder extends Seeder
 {
     public function run(): void
     {
-        $driverIds = User::pluck('id');
-
-        if ($driverIds->isEmpty()) {
-            return;
-        }
-
         $taxis = [
             ['model' => 'Dacia Logan', 'color' => 'Blanc', 'licence_plate' => '1234-A-99'],
             ['model' => 'Renault Symbol', 'color' => 'Blanc', 'licence_plate' => '5678-B-99'],
@@ -23,20 +17,21 @@ class TaxiSeeder extends Seeder
             ['model' => 'Dacia Duster', 'color' => 'Bleu', 'licence_plate' => '3456-D-99'],
             ['model' => 'Renault Clio', 'color' => 'Rouge', 'licence_plate' => '7890-E-99'],
             ['model' => 'Toyota Corolla', 'color' => 'Noir', 'licence_plate' => '1357-F-99'],
-            ['model' => 'Dacia Logan', 'color' => 'Argenté', 'licence_plate' => '2468-G-99'],
-            ['model' => 'Renault Kangoo', 'color' => 'Blanc', 'licence_plate' => '8024-H-99'],
         ];
 
-        foreach ($taxis as $taxi) {
-            Taxi::updateOrCreate(
-                ['licence_plate' => $taxi['licence_plate']],
-                [
-                    'model' => $taxi['model'],
-                    'color' => $taxi['color'],
-                    'licence_plate' => $taxi['licence_plate'],
-                    'driver_id' => $driverIds->random(),
-                ]
+        foreach ($taxis as $data) {
+            $taxi = Taxi::updateOrCreate(
+                ['licence_plate' => $data['licence_plate']],
+                ['model' => $data['model'], 'color' => $data['color']]
             );
+
+            for ($i = 1; $i <= 6; $i++) {
+                Seat::updateOrCreate(
+                    ['taxi_id' => $taxi->id, 'seat_number' => $i],
+                    ['type' => $i === 1 ? 'front' : 'back']
+                );
+            }
         }
     }
 }
+
